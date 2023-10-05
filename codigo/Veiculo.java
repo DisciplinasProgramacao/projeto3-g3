5926b96290421bd87a9b5daa362630b1cf8c3650
Ana <analuizasg52@gmail.com>
2023-10-04 22:07:15 -0300

    inicio método estacionar

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,20 +1,27 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Veiculo {
 
 	private String placa;
-	private UsoDeVaga[] usos;
+	private List <UsoDeVaga> usos;
+	private int qntdVagasUsadas;
 
 	public Veiculo(String placa) {
-		
+		this.placa = placa;
+		this.qntdVagasUsadas = 0;
 	}
 
 	public void estacionar(Vaga vaga) {
-		
-	}
+		if(vaga.disponivel()){
+				UsoDeVaga usos = new UsoDeVaga(vaga);
+				this.usos.add(usos);
+				qntdVagasUsadas++;
+			}
+		}
 
 	public double sair() {
-		
+	
 	}
 
 	public double totalArrecadado() {
