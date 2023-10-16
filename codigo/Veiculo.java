a7e2b66d67fac8a22eda1e99807e41f37ac0c54a
ana.gomes <analuizasg52@gmail.com>
2023-10-15 22:41:49 -0300

    atualização classe veículo

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,55 +1,112 @@
+import java.util.List;
 import java.util.ArrayList;
 
 public class Veiculo {
 
-	private String placa;
-	private ArrayList<UsoDeVaga> usos;
+    // Atributos
 
-	public Veiculo(String _placa) {
-		placa = _placa;
-		usos = new ArrayList<UsoDeVaga>();
-	}
+    private String placa;             // A placa do veículo
+    private List<UsoDeVaga> usos;      // Lista de usos de vagas associados ao veículo
+    private int qntdVagasUsadas;       // Quantidade de vagas usadas pelo veículo
 
-	public String getPlaca() {
-		return placa;
-	}
+    // Construtor
 
-	public ArrayList<UsoDeVaga> getUsos() {
-		return usos;
-	}
+    public Veiculo(String placa) {
+        this.placa = placa;
+        this.qntdVagasUsadas = 0;
+        this.usos = new ArrayList<>();
+    }
 
-	public void estacionar(Vaga vaga) {
+    // Métodos
 
-	}
+    /**
+     * Estaciona o veículo em uma vaga, se a vaga estiver disponível.
+     * 
+     * @param vaga A vaga em que o veículo deseja estacionar.
+     */
+    public void estacionar(Vaga vaga) {
+        if (vaga.disponivel()) {
+            UsoDeVaga uso = new UsoDeVaga(vaga);
+            this.usos.add(uso);
+            qntdVagasUsadas++;
+        }
+    }
 
-	public double sair() {
+    /**
+     * Remove o veículo de uma vaga e calcula o valor pago pelo uso da vaga.
+     * 
+     * @param vaga A vaga da qual o veículo deseja sair.
+     * @return O valor pago pelo uso da vaga ou -1 se a vaga não foi encontrada.
+     */
+    public double sair(Vaga vaga) {
+        List<UsoDeVaga> usosARemover = new ArrayList<>();
 
-	}
+        for (UsoDeVaga uso : usos) {
+            if (usos.contains(vaga)) {
+                double valorPago = uso.sair();
+                usosARemover.add(uso);
+                qntdVagasUsadas--;
+                return valorPago;
+            }
+        }
+        usos.removeAll(usosARemover);
 
-	public double totalArrecadado() {
+        return -1;
+    }
 
-	}
+    /**
+     * Calcula o valor total arrecadado com os usos de vagas pelo veículo.
+     * 
+     * @return O valor total arrecadado.
+     */
+    public double totalArrecadado() {
+        double arrecadacaoTotal = 0.0;
+        for (UsoDeVaga uso : usos) {
+            arrecadacaoTotal += uso.valorPago();
+        }
+        return arrecadacaoTotal;
+    }
 
-	public double arrecadadoNoMes(int mes) {
+    /**
+     * Calcula o valor arrecadado em um determinado mês.
+     * 
+     * @param mes O mês para o qual deseja calcular a arrecadação.
+     * @return O valor arrecadado no mês especificado.
+     */
+    public double arrecadadoNoMes(int mes) {
+        double arrecadacaoNoMes = 0.0;
+        for (UsoDeVaga uso : usos) {
+            if (uso.ehDoMes(mes)) {
+                arrecadacaoNoMes += uso.valorPago();
+            }
+        }
+        return arrecadacaoNoMes;
+    }
 
-	}
+    /**
+     * Obtém o número total de usos de vagas pelo veículo.
+     * 
+     * @return O número total de usos de vagas.
+     */
+    public int totalDeUsos() {
+        return qntdVagasUsadas;
+    }
 
-	public int totalDeUsos() {
+    // Método equals
 
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Veiculo) {
+            Veiculo veiculo = (Veiculo) obj;
+            return placa.equals(veiculo.placa);
+        }
+        return false;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (obj instanceof Veiculo) {
-			Veiculo veiculo = (Veiculo) obj;
-			return placa.equals(veiculo.getPlaca());
-		}
-		return false;
-	}
+    // Método toString
 
-	@Override
-	public String toString() {
-		return "Veículo: " + placa + " | Usos: " + usos;
-	}
-
+    @Override
+    public String toString() {
+        return "Veículo: " + placa + " | Usos: " + usos.size();
+    }
 }
