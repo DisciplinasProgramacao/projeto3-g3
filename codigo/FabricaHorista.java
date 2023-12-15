41ca57087e580fe92215ccd3d6fbef438ad42be9
Caroline Teresa <caroline.selvecki1d2019@gmail.com>
2023-12-14 22:37:21 -0300

    terminar documentacao

--- codigo/FabricaHorista.java
+++ codigo/FabricaHorista.java
@@ -1,10 +1,19 @@
 import Exceptions.VagaOcupadaException;
 
+/**
+ * Fábrica para criar instâncias de {@link UsoHorista}.
+ */
 public class FabricaHorista implements IFabrica<UsoDeVaga> {
 
+    /**
+     * Cria uma instância de {@link UsoHorista} associada à vaga especificada.
+     *
+     * @param vaga A vaga associada à instância de {@link UsoHorista} a ser criada.
+     * @return Uma instância de {@link UsoHorista}.
+     * @throws VagaOcupadaException Se a vaga estiver ocupada.
+     */
     @Override
     public UsoDeVaga create(Vaga vaga) throws VagaOcupadaException {
         return new UsoHorista(vaga);
     }
-    
 }
